3670987f39ee3092bec946cc2590a9ae9b316d1c
sairaja9 <saikraja9@gmail.com>
2019-02-12 21:19:05 -0800

    Added user input and ending messages

--- fibseq.py
+++ fibseq.py
@@ -1,10 +1,10 @@
 # Fibonacci Sequence Code In Python
 # Copyright © 2019, Sai K Raja, All Rights Reserved
 
-print("0") #Starting Fibonacci Sequence with a zero (optional)
+nth_root = input("Are you looking for a specific iteration or root of the fibonacci sequence?...If not, type 0")
 
 def fibonacci_sequence(n):  #Creating Fibonacci Sequence Function
-
+    
 
     if n == 1:   #Starting value for Fibonacci Sequence
         return 1
@@ -18,9 +18,22 @@ def fibonacci_sequence(n):  #Creating Fibonacci Sequence Function
 
 #This command builds on the new infinite values after 1 and 1 (the starting two values).
 
-for i in range(1, 15):
-    print(fibonacci_sequence(i))
+def print_sequence1():
+    for i in range(1, int(nth_root)):
+        print(fibonacci_sequence(i))
+    print("The last number in this printed sequence is the " + str(nth_root) + "th root.")
+
+
+for i in range(0):
+    print(number)
+
+number = fib_question
+
+
 
 # This for loop specifies the number of iterations python will complete.  
 # In other words, If you want the 15th number in the Fibonacci Sequence:
 # make the maximum range 15 python will run the program and give you the 15th iteration or loop.
+
+    #while number == fibonacci_sequence(i):
+     #   print(number + "is in the fibonacci seqeunce")
