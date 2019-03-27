47743e095a785326e86dc909a9def58dc2320a35
sairaja9 <saikraja9@gmail.com>
2019-03-26 20:16:03 -0700

    Now users cannot run past room 4 (no error)

--- pygame/tut5.py
+++ pygame/tut5.py
@@ -1,8 +1,7 @@
-#This game was learned from Tech With Tim's Youtube Videos
 import pygame
 pygame.init()
 
-win = pygame.display.set_mode((500, 480))
+win = pygame.display.set_mode((500,480))
 
 pygame.display.set_caption("First Game")
 
@@ -13,60 +12,66 @@ char = pygame.image.load('standing.png')
 
 clock = pygame.time.Clock()
 
+
 class player(object):
-    def __init__(self, x, y, width, height):
+    def __init__(self,x,y,width,height):
         self.x = x
         self.y = y
         self.width = width
         self.height = height
         self.vel = 5
         self.isJump = False
-        self.jumpCount = 10
         self.left = False
         self.right = False
         self.walkCount = 0
+        self.jumpCount = 10
         self.standing = True
-
 
     def draw(self, win):
         if self.walkCount + 1 >= 27:
             self.walkCount = 0
+
         if not(self.standing):
             if self.left:
-                win.blit(walkLeft[self.walkCount//3], (self.x, self.y))
+                win.blit(walkLeft[self.walkCount//3], (self.x,self.y))
                 self.walkCount += 1
             elif self.right:
-                win.blit(walkRight[self.walkCount//3], (self.x, self.y))
-                self.walkCount += 1
+                win.blit(walkRight[self.walkCount//3], (self.x,self.y))
+                self.walkCount +=1
         else:
             if self.right:
                 win.blit(walkRight[0], (self.x, self.y))
             else:
                 win.blit(walkLeft[0], (self.x, self.y))
+                
+
 
 class projectile(object):
-    def __init__(self, x, y, radius, color, facing):
+    def __init__(self,x,y,radius,color,facing):
         self.x = x
         self.y = y
         self.radius = radius
         self.color = color
         self.facing = facing
         self.vel = 8 * facing
-    
-    def draw(self, win):
-        pygame.draw.circle(win, self.color, (self.x, self.y), self.radius)
 
-man = player(300, 410, 64, 64)
-bullets = []
+    def draw(self,win):
+        pygame.draw.circle(win, self.color, (self.x,self.y), self.radius)
+
+
 
 def redrawGameWindow():
     win.blit(bg, (0,0))
     man.draw(win)
     for bullet in bullets:
         bullet.draw(win)
+    
     pygame.display.update()
 
+
 #mainloop
+man = player(200, 410, 64,64)
+bullets = []
 run = True
 while run:
     clock.tick(27)
@@ -74,7 +79,7 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-    
+        
     for bullet in bullets:
         if bullet.x < 500 and bullet.x > 0:
             bullet.x += bullet.vel
@@ -82,14 +87,15 @@ while run:
             bullets.pop(bullets.index(bullet))
 
     keys = pygame.key.get_pressed()
+
     if keys[pygame.K_SPACE]:
         if man.left:
             facing = -1
         else:
             facing = 1
-
-        if len(bullets)<5:
-            bullets.append(projectile(round(man.x + man.width//2), round(man.y + man.height//2), 6, (0,0,0), facing))
+            
+        if len(bullets) < 5:
+            bullets.append(projectile(round(man.x + man.width //2), round(man.y + man.height//2), 6, (0,0,0), facing))
 
     if keys[pygame.K_LEFT] and man.x > man.vel:
         man.x -= man.vel
@@ -103,8 +109,8 @@ while run:
         man.standing = False
     else:
         man.standing = True
-        walkCount = 0
-
+        man.walkCount = 0
+        
     if not(man.isJump):
         if keys[pygame.K_UP]:
             man.isJump = True
@@ -121,7 +127,7 @@ while run:
         else:
             man.isJump = False
             man.jumpCount = 10
-
+            
     redrawGameWindow()
 
 pygame.quit()
