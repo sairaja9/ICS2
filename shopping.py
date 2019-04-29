3a46dde8ec34b4898615da860dc80601c265a177
sairaja9 <saikraja9@gmail.com>
2019-04-29 09:21:04 -0700

    update, view item, and view list works

--- shopping.py
+++ shopping.py
@@ -1,3 +1,9 @@
+shopping_cart = [
+    ['tooth paste', 'q-tips', 'milk'],
+    ['milk', 'candy', 'apples'],
+    ['planner', 'pencils', 'q-tips']
+]
+
 welcome_text = '''
 Welcome to the shopping cart program. You can maintain several independent
 shopping lists all in one place.
@@ -18,82 +24,62 @@ You can also use these shortcuts for commands:
 
 '''
 
-# Tries to interpret a string as an integer, returns the result.
-# returns a default value if the interpretation fails.
-def safe_to_integer(value, default=0):
-  try:
-    return int(value)
-  except (ValueError, TypeError):
-    return default
-    
-# Prompts user for an integer input that must be in some range.
-# Keeps asking for a valid value until the user provides one in that range.
-# Returns: user-supplied integer. Guaranteed to be within range.
-def get_valid_integer(prompt, min_value, max_value):
-	valid = False
-	while not valid:
-		value = safe_to_integer(input(prompt + "--> "))
-		if value < min_value or value >= max_value:
-			print("Enter a value from {0} to {1}. Try again.".format(
-			    min_value, max_value - 1))
-		else:
-			valid = True
-	return value
-
 # Sets a flag to quit the next time through the main loop.
 def handle_quit():
-  global running
-  running = False
+    global running
+    running = False
 
-# Prompts the user for the location of the item to view, 
+# Prompts the user for the location of the item to view,
 # then prints it.
 #
 # TODO: Student must implement this.
 def handle_view_item():
-  item_list = input("which list is your item in?: ")
-  item = input("Which item are you looking for?: ")
+    global shopping_cart
+    list_number = int(input("Which list (number) is your item in?: "))
+    item_number = int(input('Which item number are you looking for?: '))
+    print(shopping_cart[list_number][item_number])
 
 # Prompts the user to specify a list, then prints all items
 # in that list.
 def handle_view_list():
-  print('handle_view_list() not implemented yet. Student: this is your task.')
+    list_number = int(input("Which list would you like to view?: "))
+    print(shopping_cart[list_number])
 
 # Prompts the user for a list, item index, and new item value,
-# then sets the item in the specified position to the new value.  
+# then sets the item in the specified position to the new value.
 def handle_update_item():
-  print('handle_update_item() not implemented yet. Student: this is your task.')
+    global shopping_cart
+    list_number = int(input("Which list (number) is your item in?: "))
+    item_number = int(input('Which item number are you looking for?: '))
+    new_item = input("What would you like to replace " + str(shopping_cart[list_number][item_number]) + " with?: ")
+    shopping_cart[list_number].pop(item_number)
+    shopping_cart[list_number].insert(item_number, new_item)
 
 # Gives user feedback about bad input, offers help.
 def handle_invalid_input(value):
-  print('Sorry, "' + value +'" is not a valid input.')
-  print('Type "help" if you need help')
+    print('Sorry, "' + value + '" is not a valid input.')
+    print('Type "help" if you need help')
 
 # Offers help.
 def handle_help():
-  print(help_text)
+    print(help_text)
 
-shopping_cart = [
-  ['tooth paste', 'q-tips', 'milk'],
-  ['milk', 'candy', 'apples'],
-  ['planner', 'pencils', 'q-tips']
-]
-
-## Main program
+# Main loop
 print(welcome_text)
 print(help_text)
 running = True
 while running:
-  print()
-  action = input("Choose an action --> ")
-  if action == 'view item' or action == 'vi':
-    handle_view_item()
-  elif action == 'view list' or action == 'vl':
-    handle_view_list()
-  elif action == 'update item' or action == 'ui':
-    handle_update_item()
-  elif action == 'help' or action == 'h':
-    handle_help()
-  elif action == 'quit' or action == 'q':
-    handle_quit()
-  else: 
-    handle_invalid_input(action)
+    print()
+    action = input("Choose an action --> ")
+    if action == 'view item' or action == 'vi':
+        handle_view_item()
+    elif action == 'view list' or action == 'vl':
+        handle_view_list()
+    elif action == 'update item' or action == 'ui':
+        handle_update_item()
+    elif action == 'help' or action == 'h':
+        handle_help()
+    elif action == 'quit' or action == 'q':
+        handle_quit()
+    else:
+        handle_invalid_input(action)
